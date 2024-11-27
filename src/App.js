55bb47307568ac2d91eaccc12b61ecf258f854d4
Ran Helfer <rhelfer@gmail.com>
2024-11-27 17:23:18 +0200

    adding scss style files --> index.scss and the variables.scss.
    make sure to call and use className so we scss can refer to the corresponding style and apply

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Router from "./Router";
 import "./Style/index.scss";
 
 function App() {
-    return <Router />
+    return  <div className="container"> // adding class name so we can reference this from SCSS
+        <Router />
+    </div>
+    
 }
 
 export default App;
--- src/components/Home/Snippet.js
+++ src/components/Home/Snippet.js
@@ -1,5 +1,6 @@
 import React from "react"
 import axios from "axios";
+import "./Snippet.scss";
 
 function Snippet({snippet, getSnippets, editSnippet}) {
 
@@ -9,9 +10,9 @@ function Snippet({snippet, getSnippets, editSnippet}) {
     }
 
     return <div className="snippet">
-        {snippet.title && <h2>{snippet.title}</h2>}
-        {snippet.description && <p>{snippet.description}</p>}
-        {snippet.code && <pre><code>{snippet.code}</code></pre>}
+        {snippet.title && <h2 className="title">{snippet.title}</h2>}
+        {snippet.description && <p className="description">{snippet.description}</p>}
+        {snippet.code && <pre className="code"><code>{snippet.code}</code></pre>}
         <button onClick={() =>  editSnippet(snippet)}>Edit</button>
         <button onClick={deleteSnippet}>Delete</button>
         </div>;
